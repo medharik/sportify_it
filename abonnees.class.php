6b9c4820dce6fa7d2ef2007726130b0c3f9051e1
unknown <med.harik@gmail.com>
2020-05-09 23:06:58 +0000

    creation des models

---
+++ abonnees.class.php
@@ -0,0 +1,57 @@
+<?php 
+class Abonne {
+//POJO : plain old java object
+//datas
+private $nom;
+private $prenom;
+private $date_naissance;
+private $sexe;
+private $inscrit_le;
+private $photo;
+private $cin;
+
+//constructor 
+function __construct(string $nom,string $prenom,string $date_naissance,string $sexe="femme",string $inscrit_le="",$photo,string $cin=""){
+$this->nom=$nom;
+$this->prenom=$prenom;
+$this->date_naissance=$date_naissance;
+$this->sexe=$sexe;
+if(empty($inscrit_le)){
+    $this->inscrit_le=date('Y-m-d');
+}else{
+    
+    $this->inscrit_le=$inscrit_le;
+}
+$this->inscrit_le=$inscrit_le;
+$this->photo=$photo;
+$this->cin=$cin;
+}
+// getters  
+public function nom(){
+return $nom;
+}
+//setters 
+
+public function setNom(string $nom){
+$this->nom=$nom;
+}
+//Methodes  magics :
+public  __get($nom_attribut){
+return $this->nom_attribut;
+}
+public  __set($nom_attribut,$value){
+ $this->nom_attribut=$value;
+}
+
+//methodes
+public function ajouter(){
+    
+}
+
+
+}
+
+
+
+
+?>
